f7ffccccd22d00e9f16c500c6d09bf2e34d68298
walidmohamedfarag <walid492003@gmail.com>
2025-07-27 03:32:19 +0300

    task 3 is added

---
+++ Task3/Student.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+
+namespace Task3
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int Age { get; set; }
+
+        public List<Course> Courses = new();
+
+       
+        public Student(string name, int studentId, int age)
+        {
+            Name = name;
+            StudentId = studentId;
+            Age = age;
+        }
+
+        public bool Enroll(Course course)
+        {
+            Courses.Add(course);
+            return true;
+        }
+        public string PrintDetailsOfStudent() => $"name: {Name}\nstudent id: {StudentId}\nage: {Age}\n";
+      
+
+    }
+}
