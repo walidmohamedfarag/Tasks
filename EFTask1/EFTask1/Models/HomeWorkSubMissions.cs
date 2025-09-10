ac54cb717e64c2430e92b961d6627065e3913d9d
walidmohamedfarag <walid492003@gmail.com>
2025-09-10 15:25:06 +0300

    EF Task1 Added

---
+++ EFTask1/EFTask1/Models/HomeWorkSubMissions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Models
+{
+    enum ContentType
+    {
+        Application = 1,
+        Pdf = 2,
+        Zip = 3
+    }
+    internal class HomeWorkSubMissions
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int CourseId { get; set; }
+        public DateTime SubmissionTime { get; set; }
+
+        [Unicode(false)]
+        public string Content { get; set; }
+        public ContentType ContentType { get; set; }
+        public Courses Course { get; set; }
+
+    }
+}
---
+++ EFTask1/P02_SalesDatabase/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P02_SalesDatabase.Models
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
