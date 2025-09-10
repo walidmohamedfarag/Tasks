688a344f6a37a131615c79c5b324a8f1e641f960
walidmohamedfarag <walid492003@gmail.com>
2025-09-10 15:36:57 +0300

    add P01_StudentSystem

---
+++ EFTask1/P01_StudentSystem/Models/StudentCourse.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_StudentSystem.Models
+{
+    [PrimaryKey(nameof(StudentId) , nameof(CourseId))]
+    internal class StudentCourse
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int CourseId { get; set; }
+        public Courses Course { get; set; }
+    }
+}
